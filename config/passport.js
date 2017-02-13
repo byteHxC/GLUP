db32b1fab9e81d500ed2b24a787ae45b973563bb
fblanco <fblanco@FBlanco.local>
2017-02-12 23:40:10 -0600

    Install and config passport for user sessions

---
+++ config/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport'),
+ LocalStrategy= require('passport-local').Strategy,
+ bcrypt = require('bcrypt');
+
+passport.serializeUser(function(user, done){
+	done(null,user.id);
+});
+
+passport.deserializeUser(function(id, done){
+	User.findOne({id: id}, function(id, done){
+		done(err, user);
+	});
+});
+
+passport.use(new LocalStrategy({
+	usernameField: 'email',
+	passwordField: 'password'
+	},
+	function(email, password, done){
+		User.findOne({email: email}, function(err, user){
+			if(err){return done(err)}
+			if(!user){
+				return done(null, false, {message: 'Email incorrecto'});
+			}
+			bcrypt.compare(password, user.password, function(err, res){
+				if(!res){
+					return done(null,false, {
+						message: 'Contraseña invalida'
+					})
+				}
+				var returnUser = {
+					email: user.email,
+					createdAt: user.createdAt,
+					id: user.id
+				};
+				return done(null, returnUser,{
+					message: 'Bienvenido a GLUP...'
+				});
+			});
+		});
+	}
+));
--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,16 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
+  },
+  'get /login': {
+    view: 'login'
+  },
+  'post /login': 'AuthController.login',
+  '/logout': 'AuthController.logout',
+  'get /signup': {
+    view: 'signup'
   }
+
 
   /***************************************************************************
   *                                                                          *
